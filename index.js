d297f4182c4ad81d6fced2f3e91734c642caf1a4
Rajat Kumar Singh <rajat7229@gmail.com>
2022-08-27 18:47:01 +0530

    refactored code and added some books route

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 //importing data from users.json
 const {users} = require("./data/users.json");
 
+//importing routes
+const usersRouter = require("./routes/users");  //users is js file so no need to write extenstion .js
+const booksRouter = require("./routes/books");
+
 const app = express();
 
 const PORT = 7229;
@@ -15,80 +19,8 @@ app.get("/", (req, res) => {
     });
 });
 
-app.get("/users", (req, res) => {
-    res.status(200).json({
-        success: true,
-        data: users
-    });
-});
-
-app.get("/users/:id", (req, res) => {
-    const {id} = req.params
-    const user = users.find((each) => each.id === id);
-    if(!user) {
-        return res.status(404).json({
-            success: false,
-            message: "User not found"
-        });
-    }
-    return res.status(200).json({
-        success: true,
-        data: user
-    });
-});
-
-app.post("/users", (req, res) => {
-    const {id, name, surname, email, subscriptionType, subscriptionDate} = req.body;
-    const user = users.find((each) => each.id === id);
-
-    if (user) {
-        return res.status(401).json({
-            success: "false",
-            message: "User exists with this id"
-        });
-    }
-
-    users.push({                //as key value and variable name are same we can write this way
-        id,                     //otherwise id: id,
-        name,
-        surname,
-        email,
-        subscriptionType,
-        subscriptionDate
-    });
-    return res.status(201).json({
-        success: true,
-        data: users
-    });
-});
-
-app.put("/users/:id", (req, res) => {                  //we could have used patch method also
-    const {id} = req.params;
-    const {data} = req.body;
-
-    const user = users.find((each) => each.id === id);
-
-    if (!user){
-    return res.status(404).json({
-        success: false,
-        message: "User not found"
-    });
-}
-    const updatedUser = users.map((each) => {
-        if(each.id === id){
-            return {
-                ...each,    //...for destructuring
-                ...data     //sequence of each and data matters, whatever is on top got updated by botom one
-            };
-        }
-        return each;
-    });
-
-    return res.status(201).json({
-        success: true,
-        data: updatedUser
-    });
-});
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
 
 app.get('*', (req, res) => {
     res.status(404).json({
@@ -99,3 +31,4 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
